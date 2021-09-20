fda9b7a76c8fb34b481e130d4573a08850baafaa
Simon Lyttle <srlyttle@hotmail.com>
2021-09-20 14:12:19 +0100

    server:initial grapgql resolver crud operations

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -1,16 +1,22 @@
-import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core'
+import { Field, ObjectType } from 'type-graphql'
 
+@ObjectType()
 @Entity()
 export class Post {
+  @Field()
   @PrimaryKey()
-  id!: number;
+  id!: number
 
-  @Property({ type: "date" })
-  createdAt = new Date();
+  @Field(() => String)
+  @Property({ type: 'date' })
+  createdAt = new Date()
 
-  @Property({ type: "date", onUpdate: () => new Date() })
-  updatedAt = new Date();
+  @Field(() => String)
+  @Property({ type: 'date', onUpdate: () => new Date() })
+  updatedAt = new Date()
 
-  @Property({ type: "text" })
-  title!: string;
+  @Field()
+  @Property({ type: 'text' })
+  title!: string
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,33 @@
 import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import 'reflect-metadata'
+import { buildSchema } from 'type-graphql'
+import { HelloResolver } from './resolvers/hello'
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
+import { PostsResolver } from './resolvers/post'
+import { MikroORM } from '@mikro-orm/core'
+import microConfig from './mikro-orm.config'
 
 const port = 4000
 
 const main = async () => {
   const app = express()
-
+  const orm = await MikroORM.init(microConfig)
+  await orm.getMigrator().up()
   app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
   })
-  app.get('/', (_, res) => {
-    res.send('Hello World!')
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostsResolver],
+      validate: false,
+    }),
+    // this shows the playground instead of the new sandbox
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground({})],
+    context: () => ({ em: orm.em }),
   })
+  await apolloServer.start()
+  apolloServer.applyMiddleware({ app })
 }
 main()
